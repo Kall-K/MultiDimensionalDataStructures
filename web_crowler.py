1c4d396f1d461c5284ff26f05a187db4f575efd0
Kallinikos <84861402+Kall-K@users.noreply.github.com>
2023-11-23 20:00:22 +0200

    education

--- web_crowler.py
+++ web_crowler.py
@@ -5,9 +5,13 @@
 #3)Education_and_early_life
 from bs4 import BeautifulSoup
 import requests
+
 counter = 0
 local_path = 'C:/Users/kalli/Desktop/education_url.'
+
 f = open("C:/Users/kalli/Desktop/scientist_url.txt", "r", encoding='UTF8')
+f_ = open(local_path+'txt', "a", encoding='UTF8')
+
 for url in f:
     url = url[:-1]
     page = requests.get(url)
@@ -21,16 +25,21 @@ for url in f:
     if span_tag:
         p_tag = span_tag.find_next(['ul', 'p']).get_text()
         #print(p_tag if p_tag else "No matching <p> tag found.")
-        f = open(local_path+'txt', "a", encoding='UTF8')
-        f.write(p_tag+'\n')
-        f.close()
+        #f = open(local_path+'txt', "a", encoding='UTF8')
+        f_.write(p_tag+'\n')
+        #f.close()
         
     else:
         #print("No matching <span> tag found.")
+        #f = open(local_path+'txt', "a", encoding='UTF8')
+        f_.write("No education info found.\n\n")
+        #f.close()
         counter +=1
         
     #break
 print(counter)
+f.close()
+f_.close()
 
 
 
