3ed5b342a1a01f6e647a6084e71277646ff0497d
Kallinikos <84861402+Kall-K@users.noreply.github.com>
2023-11-23 18:25:03 +0200

    education

---
+++ web_crowler.py
@@ -0,0 +1,48 @@
+#im looking for 
+#tags with:
+#1)Education
+#2)Early_life_and_education
+#3)Education_and_early_life
+from bs4 import BeautifulSoup
+import requests
+counter = 0
+local_path = 'C:/Users/kalli/Desktop/education_url.'
+f = open("C:/Users/kalli/Desktop/scientist_url.txt", "r", encoding='UTF8')
+for url in f:
+    url = url[:-1]
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    list(soup.children)
+    
+    span_tag = soup.find('span', id=['Education', 'Early_life_and_education', 'Education_and_early_life'])
+    
+    if span_tag:
+        p_tag = span_tag.find_next(['ul', 'p']).get_text()
+        #print(p_tag if p_tag else "No matching <p> tag found.")
+        f = open(local_path+'txt', "a", encoding='UTF8')
+        f.write(p_tag+'\n')
+        f.close()
+        
+    else:
+        #print("No matching <span> tag found.")
+        counter +=1
+        
+    #break
+print(counter)
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
