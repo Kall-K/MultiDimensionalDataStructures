37433b89b89825584b8ecb3fb633ae7ada1f858b
elamoresi <zikossp@gmail.com>
2023-11-25 22:11:26 +0200

    structure changes

---
+++ main.py
@@ -0,0 +1,12 @@
+# Main python script
+
+# from functions import functions.k-d.py, functions.quad.py, functions.range.py, functions.r.py, functions.lsh.py
+
+i = input("Give input")
+
+# code
+output = "Hello World!"
+# end code
+
+
+print(output)
