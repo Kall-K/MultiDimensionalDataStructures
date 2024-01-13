492357e140130666c9f6a21c2491509b98794eab
spyros-zikos <83170047+spyros-zikos@users.noreply.github.com>
2024-01-13 21:16:47 +0200

    import rtree and lsh

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-# Main python script
+from functions import str_int, r, lsh
+import os, json
 
-# from functions import functions.k-d.py, functions.quad.py, functions.range.py, functions.r.py, functions.lsh.py
+path = os.path.abspath(os.getcwd())
+with open(path + "\\data\\out2.json", "r", encoding="utf-8") as f:
+    jdata = json.load(f)
 
-i = input("Give input")
+rr = r.rtree(jdata)
+rr.build_tree()
+#rr.print_tree()
+ids = rr.search("a","z",0,0,13)
+#print(ids)
 
-# code
-output = "Hello World!"
-# end code
+educations = []
+for id in ids:
+    educations.append(jdata[id]["education"])
 
+groups = lsh.shing_minhash_lsh(educations, k=3, C=100, B=50, threshold=0.4, plot_threshold=0)
+#print(groups)
 
-print(output)
+similar_ids = []
+for g in groups:
+    id_g = []
+    for i in g:
+        id_g.append(ids[i])
+    similar_ids.append(id_g)
+
+#print(similar_ids)
+
+for sids in similar_ids:
+    if len(sids)>20:
+        continue
+    for sid in sids:
+        print(jdata[sid]["education"]+"---------------")
+    print("")
+
