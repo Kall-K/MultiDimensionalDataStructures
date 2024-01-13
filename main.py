a8eefd84bf52835de91196d5dfca018606d6db21
spyros-zikos <83170047+spyros-zikos@users.noreply.github.com>
2024-01-13 12:30:48 +0200

    main update

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 # Main python script
 
-# from functions import functions.k-d.py, functions.quad.py, functions.range.py, functions.r.py, functions.lsh.py
+#from functions import functions.k-d.py, functions.quad.py, functions.range.py, functions.r.py, functions.lsh.py
+from functions import lsh
+import os, json
 
-i = input("Give input")
-
-# code
-output = "Hello World!"
-# end code
+path = os.path.abspath(os.getcwd())
+with open(path + "\\data\\out2.json", "r", encoding="utf-8") as f:
+    jdata = json.load(f)
 
 
-print(output)
+
+data = []
+go_back = []
+for c, i in enumerate(jdata):
+    if 'education' in i.keys():
+        data.append(i['education'])
+        go_back.append(c)
+
+
+
+
+
+lsh.shing_minhash_lsh(data, k=3, C=100, B=50, threshold=0.35)
+
+
+
+
