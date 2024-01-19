7922d612af68efa80d1bef97854190b1524b17e9
Kall-K <84861402+Kall-K@users.noreply.github.com>
2024-01-19 11:17:59 +0200

    fixed kd-tree

--- functions/k_d.py
+++ functions/k_d.py
@@ -27,7 +27,7 @@ class DataPoint:
     
     def __getitem__(self, key: str):
         if key == "name":
-            return self.data[key].split()[-1]
+            return self.data[key].split()[-1].lower()
         else:
             return self.data[key]
     
@@ -51,8 +51,8 @@ class DataPoint:
 
         val = self.data[split_order[depth % 3]]
 
-        if type(val) is str:
-            return val.split()[-1] #Lastname
+        if type(val) is str: 
+            return val.split()[-1].lower() #Lastname
         else:
             return val
     
@@ -200,6 +200,11 @@ class KDTree:
         lower = low_bound[split_order[depth % 3]]
 
         check_R = check_L = False
+
+        if (type(upper) is str):
+            upper = upper.lower()
+        if (type(lower) is str):
+            lower = lower.lower()
         
         if kdNode.value(depth) >= lower: check_L = True
         else: check_R = True
@@ -239,8 +244,8 @@ if __name__ == "__main__":
     tree = KDTree(data)
     # tree.printKDTree(tree.root)
     
-    lower = {"name": "A", "dblp": 0, "awards": 0}#input
-    upper = {"name": "Zz", "dblp": 10000, "awards": 1000}#input
+    lower = {"name": "e", "dblp": 0, "awards": 0}#input
+    upper = {"name": "x", "dblp": 10000, "awards": 1000}#input
     result = []
     result = tree.rangeQuery(tree.root, 0, upper, lower, result)        
      
