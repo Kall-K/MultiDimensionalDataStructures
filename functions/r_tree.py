a2bf9b79ac878a32921a1b829941fba2738f47ef
spyros-zikos <83170047+spyros-zikos@users.noreply.github.com>
2024-02-12 20:53:08 +0200

    add stuf to main and merge str_int.py with rtree

---
+++ functions/r_tree.py
@@ -0,0 +1,268 @@
+import math, string
+
+
+# Numeric representation of a letter (1-26)
+def letter_to_int(str1):
+    let = string.ascii_lowercase
+    for i in range(len(let)):
+        if let[i] == str1:
+            return i+1
+
+# Numeric representation of a word
+def str_to_int(word):
+    word = word.lower()
+    num = 0
+
+    word = word[::-1]
+    pow = 0
+    for i in range(len(word)):
+        if word[i] not in string.ascii_lowercase:
+            num += letter_to_int('a')*(26**pow)
+            pow += 1
+        else:
+            num += letter_to_int(word[i])*(26**pow)
+            pow += 1
+    return num
+
+
+# Represents a point or a cuboid in 3d space
+class Node:
+    # Give coordinates
+    def __init__(self, minx, miny, minz, maxx, maxy, maxz, id=None):
+        self.minx = minx
+        self.miny = miny
+        self.minz = minz
+        self.maxx = maxx
+        self.maxy = maxy
+        self.maxz = maxz
+        self.id = id
+        if id == None:
+            self.children = []
+    # Get min coordinates (min distance measured from (0,0,0))
+    def getmin(self):
+        return (self.minx,self.miny,self.minz)
+    # Get max coordinates (max distance measured from (0,0,0))
+    def getmax(self):
+        return (self.maxx,self.maxy,self.maxz)
+
+# Gets json data and returns a list with points as nodes
+def get_nodelist(jdata):
+    # From json records to nodes
+    nodelist = []
+    for num,i in enumerate(jdata):
+        n1 = i['name'].split()[-1]   # Get surname from full name
+        n2 = n1.ljust(4, 'a')[:4]   # Padding so it gets correct sorting place
+        n = str_to_int(n2)   # Get a number value for the surname
+        a = i['awards']
+        d = i['dblp_records']
+        nodelist.append(Node(n, a, d, n, a, d, id=num))
+    # Get max values of each dimension
+    tmaxx, tmaxy, tmaxz = (0,0,0)
+    for i in nodelist:
+        if i.maxx > tmaxx:
+            tmaxx = i.maxx
+        if i.maxy > tmaxy:
+            tmaxy = i.maxy
+        if i.maxz > tmaxz:
+            tmaxz = i.maxz
+    # Normalize nodes' coordinates
+    for i in nodelist:
+        i.minx, i.miny, i.minz = (i.minx/tmaxx, i.miny/tmaxy, i.minz/tmaxz)
+        i.maxx, i.maxy, i.maxz = (i.maxx/tmaxx, i.maxy/tmaxy, i.maxz/tmaxz)
+
+    return (nodelist, tmaxx, tmaxy, tmaxz)
+
+# Get the Euclidean distance of 2 points in 3d space
+def dist(xyz1, xyz2):
+    return math.sqrt(math.pow(xyz1[0]-xyz2[0], 2) + 
+                     math.pow(xyz1[1]-xyz2[1], 2) + 
+                     math.pow(xyz1[2]-xyz2[2], 2))
+
+# Get index of a point in nodelist that is nearest to the point with coordinates xyz
+# xyz is like (x,y,z)
+def nearest(xyz, nodelist):
+    indx = 0
+    for i in range(len(nodelist)):
+        if (dist(nodelist[i].getmin(), xyz) < 
+            dist(nodelist[indx].getmin(), xyz)):
+            indx = i
+    return indx
+
+# Make a cuboid from nodes in nodelist
+# Set the coordinates so that the cuboid encompases all the nodes
+# Add to the cuboid's children the nodes of the nodelist
+def makembr(nodelist):
+    bignum = 100000000000
+    mbr = Node(bignum,bignum,bignum,0,0,0)
+    for i in nodelist:
+        if i.minx < mbr.minx:
+            mbr.minx = i.minx
+        if i.miny < mbr.miny:
+            mbr.miny = i.miny
+        if i.minz < mbr.minz:
+            mbr.minz = i.minz
+        if i.maxx > mbr.maxx:
+            mbr.maxx = i.maxx
+        if i.maxy > mbr.maxy:
+            mbr.maxy = i.maxy
+        if i.maxz > mbr.maxz:
+            mbr.maxz = i.maxz
+    for i in nodelist:
+        mbr.children.append(i)
+    return mbr
+
+# Give list with nodes-points. Get a list with the root node
+def build(nodelist, K):
+    nodes_for_mbr = nodelist   # List with nodes to put in MBRs
+    list_with_mbrs = []   # List with final list with MBRs
+    start = (0,0,0)
+
+    if len(nodelist) == 1:
+        return nodelist
+
+    local_list = []   # Temporary storage for each MBR
+    while len(nodes_for_mbr) > 0:
+        # if we need to create an MBR
+        if (len(local_list) == K-1) or (len(nodes_for_mbr) == 1):
+            near_idx = nearest(start,nodes_for_mbr)
+            near = nodes_for_mbr.pop(near_idx)
+            local_list.append(near)
+
+            new_node = makembr(local_list)
+            list_with_mbrs.append(new_node)
+            local_list = []
+            start = (0,0,0)
+        # if we want to append a node to nodelist for the next MBR
+        else:
+            near_idx = nearest(start,nodes_for_mbr)
+            near = nodes_for_mbr.pop(near_idx)
+            local_list.append(near)
+            start = near.getmin()
+    return build(list_with_mbrs, K)
+
+
+def mbr_intersects_mbr(big_mbr, small_mbr):
+    if ((
+        (big_mbr.minx<=small_mbr.minx and big_mbr.maxx>=small_mbr.minx) or
+        (big_mbr.maxx>=small_mbr.maxx and big_mbr.minx<=small_mbr.maxx) or
+        (big_mbr.minx>=small_mbr.minx and big_mbr.maxx<=small_mbr.maxx)
+        ) and (
+        (big_mbr.miny<=small_mbr.miny and big_mbr.maxy>=small_mbr.miny) or
+        (big_mbr.maxy>=small_mbr.maxy and big_mbr.miny<=small_mbr.maxy) or
+        (big_mbr.miny>=small_mbr.miny and big_mbr.maxy<=small_mbr.maxy)
+        ) and (
+        (big_mbr.minz<=small_mbr.minz and big_mbr.maxz>=small_mbr.minz) or
+        (big_mbr.maxz>=small_mbr.maxz and big_mbr.minz<=small_mbr.maxz) or
+        (big_mbr.minz>=small_mbr.minz and big_mbr.maxz<=small_mbr.maxz)
+        )):
+        return 1
+    return 0
+
+
+def get_results(root,minx=0,miny=0,minz=0,maxx=1,maxy=1,maxz=1):
+    results = []
+    def search(root,minx,miny,minz,maxx,maxy,maxz):
+        for i in root:
+            if i.id != None:
+                if ((i.minx>=minx) and (i.miny>=miny) and (i.minz>=minz) and
+                    (i.minx<=maxx) and (i.miny<=maxy) and (i.minz<=maxz)):
+                    results.append(i.id)
+            else:
+                if mbr_intersects_mbr(Node(minx,miny,minz,maxx,maxy,maxz,0), i):
+                    search(i.children, minx,miny,minz,maxx,maxy,maxz)
+
+    search(root,minx,miny,minz,maxx,maxy,maxz)
+    return results
+
+
+def printdata(data, root, show_points=0, show_rects=2):
+    """
+    show_rects: if 0 it will NOT show rects,
+                if 1 it will show ALL rects,
+                if 2 it will show final rects
+    """
+    import matplotlib.pyplot as plt
+
+    ax = plt.figure().add_subplot(projection='3d')
+
+    # Plot points
+    xs, ys, zs = ([], [], [])
+    for i in data:
+        xs.append(i.minx)
+        xs.append(i.maxx)
+        ys.append(i.miny)
+        ys.append(i.maxy)
+        zs.append(i.minz)
+        zs.append(i.maxz)
+    if show_points:
+        ax.scatter(xs, ys, zs)
+
+    # Plot rectangles
+    def plot_rect_xyz(cmin=(0,0,0), cmax=(1,1,1)):
+        x = [cmin[0], cmax[0], cmax[0], cmin[0], cmin[0],   cmin[0],   cmin[0], cmax[0], cmax[0], cmin[0], cmin[0],    cmax[0], cmax[0],    cmax[0], cmax[0],   cmin[0], cmin[0]]
+        y = [cmin[1], cmin[1], cmax[1], cmax[1], cmin[1],   cmin[1],   cmin[1], cmin[1], cmax[1], cmax[1], cmin[1],    cmin[1], cmin[1],    cmax[1], cmax[1],   cmax[1], cmax[1]]
+        z = [cmin[2], cmin[2], cmin[2], cmin[2], cmin[2],   cmax[2],   cmax[2], cmax[2], cmax[2], cmax[2], cmax[2],    cmax[2], cmin[2],    cmin[2], cmax[2],   cmax[2], cmin[2]]
+        return [x,y,z]
+    
+    rect_list = []
+    def get_rects(root):
+        if show_rects==1:
+            for i in root:
+                if i.id == None:
+                    rect_list.append([i.getmin(), i.getmax()])
+                    get_rects(i.children)
+        else:
+            for i in root:
+                if i.id == None:
+                    if i.children[0].id != None:
+                        rect_list.append([i.getmin(), i.getmax()])
+                    get_rects(i.children)
+    
+    if show_rects > 0:
+        get_rects(root)
+        for i in rect_list:
+            x,y,z = plot_rect_xyz(i[0],i[1])
+            ax.plot(x, y, z, zdir='z')
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    plt.show()
+
+
+class Rtree:
+    def __init__(self, jdata, K=5):
+        (self.nodelist, self.tmaxx, self.tmaxy, self.tmaxz) = get_nodelist(jdata)
+        self.K = K
+    def build_tree(self):
+        self.root = build(self.nodelist.copy(), self.K)
+
+    def search(self, n1, n2, a, d1, d2):   # use: search("a","c",0,0,2)
+        n1_ = str_to_int(n1.ljust(4, 'a')[:4])
+        n2_ = str_to_int(n2.ljust(4, 'z')[:4])
+        return get_results(self.root, minx=n1_/self.tmaxx, miny=a/self.tmaxy,
+                    minz=d1/self.tmaxz, maxx=n2_/self.tmaxx, maxz=d2/self.tmaxz)
+    def print_tree(self):
+        printdata(self.nodelist, self.root)
+
+
+if __name__ == "__main__":
+    import os, json
+
+    path = os.path.abspath(os.getcwd())
+    end1 = "\\..\\data\\out2.json"
+    end2 = "\\data\\out2.json"
+    with open(path + end2, "r", encoding="utf-8") as f:
+        jdata = json.load(f)
+    
+    rr = Rtree(jdata)
+    rr.build_tree()
+    #rr.print_tree()
+    n1 = "a"
+    n2 = "e"
+    a1 = 0
+    d1 = 0
+    d2 = 10000
+    ids = rr.search(n1,n2,a1,d1,d2)
+    print(len(ids))
+
--- main.py
+++ main.py
@@ -1,76 +1,133 @@
-from functions import k_d, quad, range, rtree, str_int, lsh
-import os, json
-from timeit import default_timer as timer
+from functions import kd_tree, quad_tree, range_tree, r_tree, lsh
 from time import perf_counter
-
-path = os.path.abspath(os.getcwd())
-with open(path + "\\data\\out2.json", "r", encoding="utf-8") as f:
-    jdata = json.load(f)
-
-
-n1 = "A"
-n2 = "zzzzzzzzzz"
-a1 = 0
-d1 = 0
-d2 = 10000
-
-#tic = timer()
-t0 = perf_counter()
-rr = rtree.rtree(jdata)
-rr.build_tree()
-t1 = perf_counter()
-print(f"R-Tree construction time: {t1-t0}")
-
-t0 = perf_counter()
-ids = rr.search(n1,n2,a1,d1,d2)
-t1 = perf_counter()
-print(f"R-Tree query time: {t1-t0}\n")
+from matplotlib import pyplot as plt
+import os, json, string, random
+import pandas as pd
 
 
-t0 = perf_counter()
-tree_range = range.RangeTree(jdata)
-t1 = perf_counter()
-print(f"Range Tree construction time: {t1-t0}")
-t0 = perf_counter()
-ids_range = tree_range.query_driver([n1,n2],[a1,None],[d1,d2])
-t1 = perf_counter()
-print(f"Range Tree query time: {t1-t0}\n")
+max_a = 0
+
+#=========================================================================================
+
+def build_trees(jdata):
+    
+    global max_a
+
+    runtimes = {}
+    trees = {}
+
+    # R-Tree construction
+    t0 = perf_counter()
+    rr = r_tree.Rtree(jdata)
+    rr.build_tree()
+    t1 = perf_counter()
+    runtimes["r_tree"] = t1-t0
+    trees["r_tree"] = rr
+
+    # Range-Tree construction
+    t0 = perf_counter()
+    trees["range_tree"] = range_tree.RangeTree(jdata)
+    t1 = perf_counter()
+    runtimes["range_tree"] = t1-t0
+
+    # Quad-Tree construction
+    t0 = perf_counter()
+    trees["quad_tree"], max_a = quad_tree.init_quadTree(jdata)
+    t1 = perf_counter()
+    runtimes["quad_tree"] = t1-t0
+
+    # Kd-Tree construction
+    t0 = perf_counter()
+    trees["kd_tree"] = kd_tree.KDTree(jdata)
+    t1 = perf_counter()
+    runtimes["kd_tree"] = t1-t0
+
+    return trees, runtimes
+
+#=========================================================================================
+
+def query_all(trees, n1, n2, a1, d1, d2):
+    
+    runtimes = {}
+
+    rr = trees["r_tree"]
+    tree_range = trees["range_tree"]
+    octree = trees["quad_tree"]
+    k_dtree = trees["kd_tree"]
 
 
-#toc = timer()
+    # R-Tree query
+    t0 = perf_counter()
+    ids_rtree = rr.search(n1,n2,a1,d1,d2)
+    t1 = perf_counter()
+    runtimes["r_tree"] = t1-t0
+    
+    # Range Tree query
+    t0 = perf_counter()
+    ids_range = tree_range.query_driver([n1,n2],[a1,None],[d1,d2])
+    t1 = perf_counter()
+    runtimes["range_tree"] = t1-t0
 
-t0 = perf_counter()
-octree, max_x = quad.init_quadTree(jdata)
-t1 = perf_counter()
-print(f"Quad Tree construction time: {t1-t0}")
+    # Quad Tree query
+    t0 = perf_counter()
+    ids_quad = octree.range_query(octree.root, [[a1,max_a],[d1,d2],[quad_tree.str_to_int(n1, "a"),quad_tree.str_to_int(n2, "z")]], [])
+    t1 = perf_counter()
+    runtimes["quad_tree"] = t1-t0
 
-t0 = perf_counter()
-ids_quad = octree.range_query(octree.root, [[a1,max_x],[d1,d2],[quad.str_to_int(n1, "a"),quad.str_to_int(n2, "z")]], [])
-t1 = perf_counter()
-print(f"Quad Tree query time: {t1-t0}\n")
+    lower = {"name":n1, "dblp":d1, "awards":a1}
+    upper = {"name":n2,"dblp":d2, "awards":max_a}
+    t0 = perf_counter()
+    ids_kd = k_dtree.rangeQuery(k_dtree.root, 0, upper, lower, [])
+    t1 = perf_counter()
+    runtimes["kd_tree"] = t1-t0
 
+    assert ids_rtree.sort() == ids_range.sort(), "Range tree ids not equal to R-tree ids"
+    assert ids_rtree.sort() == ids_quad.sort(), "Range tree ids not equal to R-tree ids"
+    assert ids_rtree.sort() == ids_kd.sort(), "Range tree ids not equal to R-tree ids"
 
+    return runtimes, ids_rtree
 
+#=========================================================================================
 
-k_dtree = k_d.KDTree(jdata)
-lower = {"name": n1.lower(), "dblp": d1, "awards": a1}
-upper = {"name": n2.lower(), "dblp": d2, "awards": max_x}
-result = k_dtree.rangeQuery(k_dtree.root, 0, upper, lower, [])
-print(len(result))
+def run_experiments(trees, num_of_experiments=10):
+    #rtree, range, quad, kd
+    runtimes = {"parameters": [], "ids": [], "r_tree": [], "range_tree": [], "quad_tree": [], "kd_tree": []}
+    for _ in range(num_of_experiments):
+        n1 = random.choice(string.ascii_lowercase)
+        n2 = random.choice(n1 + string.ascii_lowercase.split(n1)[1])
+        a1 = random.randint(0,15)
+        d1 = random.randint(0,500)
+        d2 = random.randint(d1,500)
+        runtimes["parameters"].append({"n1": n1, "n2": n2, "a1": a1, "d1": d1, "d2": d2})
+        query_times, ids = query_all(trees, n1, n2, a1, d1, d2)
+        runtimes["ids"].append(ids)
 
+        runtimes["r_tree"].append(query_times["r_tree"])
+        runtimes["range_tree"].append(query_times["range_tree"])
+        runtimes["quad_tree"].append(query_times["quad_tree"])
+        runtimes["kd_tree"].append(query_times["kd_tree"])
 
-print(len(ids_quad))
+    return runtimes
 
+#=========================================================================================
 
-print(((ids.sort()==ids_range.sort()) and (ids_range.sort()==ids_quad.sort())))  # put kd ids
+def unique_range_query(trees):
+    n1 = input("Give min name letter: ")
+    n2 = input("Give max name letter: ")
+    a1 = input("Give min awards number: ")
+    d1 = input("Give min dblp record number: ")
+    d2 = input("Give max dblp record number: ")
 
-def edu():
+    return query_all(trees, n1, n2, int(a1), int(d1), int(d2))
+
+#=========================================================================================
+
+def lsh_run(ids):
     educations = []
     for id in ids:
         educations.append(jdata[id]["education"])
 
     groups = lsh.shing_minhash_lsh(educations, k=3, C=100, B=50, threshold=0.3, plot_threshold=0)
-    print(groups)
 
     similar_ids = []
     for g in groups:
@@ -79,12 +136,100 @@ def edu():
             id_g.append(ids[i])
         similar_ids.append(id_g)
 
-    #print(similar_ids)
+    with open("lsh.txt", "w") as f:
+        for sids in similar_ids:
+            if len(sids)>20:
+                continue
+            for sid in sids:
+                f.write(str(sid)+": "+jdata[sid]["name"]+"\n")
+            f.write("\n")
 
-    for sids in similar_ids:
-        if len(sids)>20:
-            continue
-        for sid in sids:
-            print(jdata[sid]["education"]+"---------------")
-        print("")
+#=========================================================================================
 
+if __name__ == "__main__":
+
+    #Read data from json file
+    jdata = None
+
+    path = os.path.abspath(os.getcwd())
+    with open(path + "\\data\\out2.json", "r", encoding="utf-8") as f:
+        jdata = json.load(f)
+
+    #Number of queries executed
+    print("If you set num of range queries equal to 1 you give the inputs manually \notherwise the parameters are configured automatically.\n")
+    num_experiments = int(input("Select the number of range queries to be executed: "))
+    #Build trees
+    trees, build_times = build_trees(jdata)
+
+    if (num_experiments > 1):
+        #Execute random queries
+        result = run_experiments(trees, num_experiments)
+        
+        #Bar plot of average construction times
+        data = {"R-Tree": build_times["r_tree"], "Range Tree": build_times["range_tree"], "Quad Tree": build_times["quad_tree"], "KD Tree": build_times["kd_tree"]}
+        trees = list(data.keys())
+        times = list(data.values())
+
+        fig = plt.figure(figsize = (10, 5))
+
+        # creating the bar plot
+        plt.bar(trees, times, color ='maroon', width = 0.4)
+
+        plt.xlabel("Tree")
+        plt.ylabel("Time")
+        plt.title("Average construction times for all trees")
+    
+
+        #Average query times for each tree
+        r_time = sum(result["r_tree"])/num_experiments
+        range_time = sum(result["range_tree"])/num_experiments
+        quad_time = sum(result["quad_tree"])/num_experiments
+        kd_time = sum(result["kd_tree"])/num_experiments
+
+        print(f"Average R-Tree time: {r_time}")
+        print(f"Average Range Tree time: {range_time}")
+        print(f"Average Quad-Tree time: {quad_time}")
+        print(f"Average KD-Tree time: {kd_time}")
+
+        for i, t in enumerate(result["r_tree"]):
+            print(f"Time of R-Tree for query {i}: {t}")
+
+        #Bar plot of average times for queries
+        data = {"R-Tree": r_time, "Range Tree": range_time, "Quad Tree": quad_time, "KD Tree": kd_time}
+        trees = list(data.keys())
+        times = list(data.values())
+
+        fig = plt.figure(figsize = (10, 5))
+
+        # creating the bar plot for queries
+        plt.bar(trees, times, color ='maroon', width = 0.4)
+
+        plt.xlabel("Tree")
+        plt.ylabel("Time")
+        plt.title("Average query times for all trees")
+        plt.show()
+
+        #Export times to excel
+        excel_data = {"R-Tree": result["r_tree"], "Range Tree": result["range_tree"], "Quad Tree": result["quad_tree"], "KD Tree": result["kd_tree"]}
+        df = pd.DataFrame(excel_data, index=list(range(num_experiments)))
+        df.to_excel("query_times.xlsx", index_label="Query")
+
+        #Export queries to excel
+        excel_queries = {"n1": [], "n2": [], "a1": [], "d1": [], "d2": []}
+
+        for query in result["parameters"]:
+            for key, value in query.items():
+                excel_queries[key].append(value)
+
+        df_queries = pd.DataFrame(excel_queries, index=list(range(num_experiments)))
+        df_queries.to_excel("queries.xlsx", index_label = "Query")
+    elif (num_experiments == 1):
+        times, ids = unique_range_query(trees)
+
+        print(f"Average R-Tree time: {times['r_tree']}")
+        print(f"Average Range Tree time: {times['range_tree']}")
+        print(f"Average Quad-Tree time: {times['quad_tree']}")
+        print(f"Average KD-Tree time: {times['kd_tree']}")
+
+        lsh_run(ids)
+    else: print("Invalid Input, Pls try again!")
